0746d721b8ad1836ffff7fbc4a3cb73ff3ec5924
che <che-email@yandex.ru>
2021-02-19 13:44:42 +0500

    Добавили бутстрап и базовый шаблон. А также добавили поле ответа в модель задачи

--- exerciseapp/models.py
+++ exerciseapp/models.py
@@ -6,6 +6,11 @@ from django.urls import reverse
 class Exercise(models.Model):
     name = models.CharField(max_length=255,verbose_name="Название задачи")
     text = models.TextField(verbose_name="Полный текст задачи в формате LaTeX")
+    solution = models.TextField(
+        verbose_name="Полный текст решения в формате LaTeX",
+        blank=True
+    )
+    answer = models.CharField(verbose_name="Правильный ответ",max_length=255,blank=True)
     score = models.IntegerField(blank=True,default=0)
     def get_absolure_url(self):
         return reverse('exercise-detail',args=[self.id])
